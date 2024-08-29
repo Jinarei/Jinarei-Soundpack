3d96d28e14bce371e6b7154a6e8315ab2b44cec3
Jinarei <58641200+Jinarei@users.noreply.github.com>
2024-08-29 03:31:49 +0200

    Ajout fichier

--- Jinarei-Soundpack.lua
+++ Jinarei-Soundpack.lua
@@ -1,6 +1,12 @@
 SoundLibrary = {
-    ["Son 1"] = "Interface\\AddOns\\SoundLibrary\\Sounds\\mon_son1.mp3",
-    ["Son 2"] = "Interface\\AddOns\\SoundLibrary\\Sounds\\mon_son2.mp3",
+    ["bloodlust"] = "Interface\\AddOns\\SoundLibrary\\Sounds\\bloodlust.mp3",
+    ["bloodlust2"] = "Interface\\AddOns\\SoundLibrary\\Sounds\\bloodlust2.mp3",
+    ["bloodlust3"] = "Interface\\AddOns\\SoundLibrary\\Sounds\\bloodlust3.mp3",
+    ["bloodlust4"] = "Interface\\AddOns\\SoundLibrary\\Sounds\\bloodlust4.mp3",
+    ["bloodlust5"] = "Interface\\AddOns\\SoundLibrary\\Sounds\\bloodlust5.mp3",
+    ["bloodlust6"] = "Interface\\AddOns\\SoundLibrary\\Sounds\\bloodlust6.mp3",
+    ["bloodlust7"] = "Interface\\AddOns\\SoundLibrary\\Sounds\\bloodlust7.mp3",
+    ["bloodlust8"] = "Interface\\AddOns\\SoundLibrary\\Sounds\\bloodlust8.mp3",
 }
 
 function GetSoundPath(soundName)
