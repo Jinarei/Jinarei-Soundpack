2f2f44fee3f5073fa83c311f8867cd180e95717b
Jinarei <58641200+Jinarei@users.noreply.github.com>
2024-09-21 15:14:06 +0200

    ajout son bl + fireball

--- Jinarei-Soundpack.lua
+++ Jinarei-Soundpack.lua
@@ -39,4 +39,8 @@ LSM:Register("sound", "|cFF00FF00tsunami|r", [[Interface\Addons\Jinarei-Soundpac
 LSM:Register("sound", "|cFF00FF00pause|r", [[Interface\Addons\Jinarei-Soundpack\Sounds\pause.ogg]])
 LSM:Register("sound", "|cFF00FF00lossgets|r", [[Interface\Addons\Jinarei-Soundpack\Sounds\lossgets.ogg]])
 LSM:Register("sound", "|cFF00FF00uwu|r", [[Interface\Addons\Jinarei-Soundpack\Sounds\uwu.ogg]])
+LSM:Register("sound", "|cFF00FF00fireball|r", [[Interface\Addons\Jinarei-Soundpack\Sounds\fireball.ogg]])
+LSM:Register("sound", "|cFF00FF00bloodlust9|r", [[Interface\Addons\Jinarei-Soundpack\Sounds\bloodlust9.ogg]])
+LSM:Register("sound", "|cFF00FF00bloodlust10|r", [[Interface\Addons\Jinarei-Soundpack\Sounds\bloodlust10.ogg]])
+LSM:Register("sound", "|cFF00FF00bloodlust11|r", [[Interface\Addons\Jinarei-Soundpack\Sounds\bloodlust11.ogg]])
 
